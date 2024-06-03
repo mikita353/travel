e5ec271b065cb6593dadfbe2112547e42d1759dd
Nitin Bahadur <nitinb@gmail.com>
2024-06-02 19:01:07 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,20 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import langchain_helper
 
-"""
-# Welcome to Streamlit!
+st.title('Baby Name Generator')
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+gender = st.sidebar.selectbox("Choose a gender",
+                             ("Girl", "Boy"))
+nationality = st.sidebar.selectbox("Choose the nationality", 
+                                  ("American", "Indian", "Chinese", 
+                                  "Russian")
+                                )
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 
-num_points = st.slider("Number of points in spiral", 1, 1000, 55)
-num_turns = st.slider("Number of turns in spiral", 1, 27, 6)
+if gender and nationality:
+    response = langchain_helper.generate_baby_names(gender, nationality)
+    baby_names = response['baby_names'].strip().split(",")
+    st.write("** Top 5 Baby Names **")
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+    for name in baby_names:
+        st.write("--", name)
