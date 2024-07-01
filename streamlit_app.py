5ad9ec4fdfec381588e6e5fb0976b9ea9ee13803
Nikki <139157155+mikita353@users.noreply.github.com>
2024-06-30 19:13:10 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from langchain.llms import OpenAI
+from openai import OpenAI
+#from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.chains import SequentialChain
